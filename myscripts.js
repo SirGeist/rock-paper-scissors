89493c800a9476fd57d4fdd91c63a5891811533b
SirGeist <trinh1234533@yahoo.com>
2024-03-03 20:55:42 -0600

    Added computer chooses an option

---
+++ myscripts.js
@@ -0,0 +1,35 @@
+// Function that randomly chooses rock, paper, or scissors;
+// It then returns that value
+let getComputerChoice = () => {
+  // Generating a random number between 0-2 to assign
+  // as our choices
+  let choice = Math.floor(Math.random() * 3);
+
+  // Assigning each choice
+  switch (choice) {
+    case 0:
+      choice = "rock";
+      break;
+    case 1:
+      choice = "paper";
+      break;
+
+    case 2:
+      choice = "scissors";
+      break;
+
+    default:
+      console.log("Error");
+  }
+
+  return choice;
+};
+
+getComputerChoice();
+
+// Getting user input
+var input = prompt("What is your choice? Rock, Paper, or Scissors?");
+
+let playGame = (playerSelection, computerSelection) => {};
+
+//playGame(,getComputerChoice());
