be5c63c2c04b593bd928f6792888c12a3884e150
SirGeist <trinh1234533@yahoo.com>
2024-03-03 21:32:35 -0600

    Completed the game function; checks for valid input; then displays who wins given the selections

--- myscripts.js
+++ myscripts.js
@@ -30,9 +30,46 @@ getComputerChoice();
 // Getting user input
 var playerSelection = prompt("What is your choice? Rock, Paper, or Scissors?");
 
+// Function that takes in both user and computer input to determine who wins
 let playGame = (playerSelection, computerSelection) => {
+  // We first lowercase the playerSelection so we can compare to the computer
+  // Display error if incorrect input
   playerSelection = playerSelection.toLowerCase();
-  console.log(playerSelection);
+  if (
+    playerSelection != "rock" &&
+    playerSelection != "paper" &&
+    playerSelection != "scissors"
+  ) {
+    console.log("Error! Incorrect input!");
+    return;
+  }
+
+  // Display a tie if both selections are the same
+  if (playerSelection == computerSelection) {
+    console.log("It's a tie! You both chose " + playerSelection + "!");
+  } else {
+    switch (true) {
+      // If the player wins, appropriately display that information and tell them that they won
+      case (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "rock" && computerSelection == "scissors"):
+        console.log(
+          playerSelection + " beats " + computerSelection + "! You win!"
+        );
+        break;
+
+      // Otherwise, display that they lost
+      default:
+        console.log(
+          "Computer chose " +
+            computerSelection +
+            ". You chose " +
+            playerSelection +
+            ". You lose"
+        );
+    }
+  }
 };
 
+// Calling the function
 playGame(playerSelection, getComputerChoice());
